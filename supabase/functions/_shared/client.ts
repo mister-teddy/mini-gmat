6b8a12d9dc1b4672161e4241d663d19e84de3201
nguyenhongphat0 <hongphat.js@gmail.com>
2023-03-27 20:48:38 +0700

    init client with edge functions default env

---
+++ supabase/functions/_shared/client.ts
@@ -0,0 +1,9 @@
+import { createClient } from "https://deno.land/x/supabase/mod.ts";
+
+const supabaseUrl = Deno.env.get("SUPABASE_URL")!;
+const supabaseKey = Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!;
+
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  detectSessionInUrl: false,
+  persistSession: false,
+});
