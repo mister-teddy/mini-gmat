f1232a00f108b64f904f842ac96367ec7febf586
nguyenhongphat0 <hongphat.js@gmail.com>
2023-04-25 16:19:01 +0700

    use schema GMAT

--- supabase/functions/_shared/client.ts
+++ supabase/functions/_shared/client.ts
@@ -6,4 +6,5 @@ const supabaseKey = Deno.env.get("SUPABASE_SERVICE_ROLE_KEY")!;
 export const supabase = createClient(supabaseUrl, supabaseKey, {
   detectSessionInUrl: false,
   persistSession: false,
+  schema: "gmat",
 });
